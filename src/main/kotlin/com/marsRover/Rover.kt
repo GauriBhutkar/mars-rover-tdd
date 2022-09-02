446da4eb4153cf32d9f0919a510bc5d0df69dc0e
Gauri Manoj Bhutkar <g.bhutkar@ALEFs-MacBook-Pro-5.local>
2022-09-02 21:05:09 +0530

    comsetic changes

--- src/main/kotlin/com/marsRover/Rover.kt
+++ src/main/kotlin/com/marsRover/Rover.kt
@@ -4,15 +4,10 @@ class Rover {
     private var direction = "N"
     fun execute(commands: String): String {
         for (command in commands.toCharArray()) {
-            if (direction == "N") {
-                direction = "E"
-            } else if (direction == "E") {
-                direction = "S"
-            } else if (direction == "S") {
-                direction = "W"
-            } else if (direction == "W") {
-                direction = "N"
-            }
+            if (direction == "N") direction = "E"
+            else if (direction == "E") direction = "S"
+            else if (direction == "S") direction = "W"
+            else direction = "N"
         }
         return "0:0:$direction"
     }
