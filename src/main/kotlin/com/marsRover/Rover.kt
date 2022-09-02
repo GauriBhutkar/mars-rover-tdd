7a283b5463524b84bab0cad945f69f932c62d3d8
Gauri Manoj Bhutkar <g.bhutkar@ALEFs-MacBook-Pro-5.local>
2022-09-02 20:53:43 +0530

    support more than one command to rotate rover to right

--- src/main/kotlin/com/marsRover/Rover.kt
+++ src/main/kotlin/com/marsRover/Rover.kt
@@ -1,7 +1,19 @@
 package com.marsRover
 
 class Rover {
+    private var direction = "N"
     fun execute(commands: String): String {
-        return "0:0:E"
+        for (command in commands.toCharArray()) {
+            if (direction == "N") {
+                direction = "E"
+            } else if (direction == "E") {
+                direction = "S"
+            } else if (direction == "S") {
+                direction = "W"
+            } else if (direction == "W") {
+                direction = "N"
+            }
+        }
+        return "0:0:$direction"
     }
 }
--- src/test/kotlin/com/marsRover/RoverTest.kt
+++ src/test/kotlin/com/marsRover/RoverTest.kt
@@ -1,13 +1,31 @@
 package com.marsRover
 
 import io.kotlintest.shouldBe
-import org.junit.jupiter.api.Test
+import junitparams.JUnitParamsRunner
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
-internal class RoverTest {
-    @Test
-    fun `should rotate rover to right`() {
-        val rover = Rover()
+import org.junit.runner.RunWith
 
-        rover.execute("R") shouldBe "0:0:E"
+@RunWith(JUnitParamsRunner::class)
+class RoverTest {
+
+    private lateinit var rover: Rover
+
+    @BeforeEach
+    fun initialise(){
+        rover = Rover()
+    }
+
+    @ParameterizedTest(name = "should rotate rover to {1} if command given is {0}")
+    @CsvSource(value = [
+        "R, 0:0:E",
+        "RR, 0:0:S",
+        "RRR, 0:0:W",
+        "RRRR, 0:0:N"
+    ])
+    fun `should rotate rover to right`(commands: String, position: String) {
+        rover.execute(commands) shouldBe position
     }
 }
